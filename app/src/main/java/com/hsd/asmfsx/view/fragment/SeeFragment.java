5cc18a3d1d34e966bcf29674ba880cb24739cb43
sunzhan <675650245@qq.com>
2016-11-04 21:24:13 +0800

    2016年11月04日21:23:56  增加tabhost

---
+++ app/src/main/java/com/hsd/asmfsx/view/fragment/SeeFragment.java
@@ -0,0 +1,23 @@
+package com.hsd.asmfsx.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hsd.asmfsx.R;
+
+/**
+ * Created by apple on 2016/11/4.
+ */
+
+public class SeeFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.see_layout, container, false);
+        return view;
+    }
+}
